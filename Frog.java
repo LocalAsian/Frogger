405aa04a4d4a6028fcdaa7f7ceb990a92b6305b8
hn300371 <142824258+hn300371@users.noreply.github.com>
2023-08-31 16:48:13 +0000

    a

--- Frog.java
+++ Frog.java
@@ -14,4 +14,18 @@ private VisibleImage frogImage;
   public boolean overlaps(VisibleImage vehicleImage){
       return frogImage.overlaps(vehicleImage);
   }
+  public void hops(String direction){
+    if(direction=="w"){
+      frogImage.move(0.0,50.0);
+    }
+    else if(direction=="d"){
+      frogImage.move(50.0,0.0);
+    }
+    else if(direction=="a"){
+      frogImage.move(-50.0,0.0);
+    }
+    else if(direction=="s"){
+      frogImage.move(0.0,-50.0);
+    }
+  }
 }
