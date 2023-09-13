4406e8470b853a3773f0553fe931448c2b0e627e
LocalAsian <142824171+LocalAsian@users.noreply.github.com>
2023-09-13 15:26:47 +0000

    working death
    w

--- Vehicle.java
+++ Vehicle.java
@@ -3,33 +3,26 @@ import java.awt.*;
 import java.awt.Color;
 public class Vehicle extends ActiveObject{
 	private VisibleImage vehicleImage; 
-	private double speed;
+	private double Speed;
 	private Frog fred;
 	public Vehicle(Image i,int x,int y,DrawingCanvas c,double speed, Frog fred) {
 		vehicleImage = new VisibleImage(i, new Location(800,45), c);
 		vehicleImage.setSize(110.0,60.0);
-		speed = -5.0;
+		Speed = -5.0;
 		this.fred = fred;
 		this.start();
 	}
-
-	public Vehicle(Image i,int x,int y,DrawingCanvas c)
-	{
-		vehicleImage = new VisibleImage(i, new Location(200,160), c);
-		
-		speed = 70.0;
-	}
-    
 	public void run(){
     
 		double currentTime = System.currentTimeMillis();
 		pause(20.0);
 		double actualPause = currentTime-50;
-		double distance = actualPause*speed;
-		while (vehicleImage.getX()>-100 && vehicleImage.getX()>1000) {
-			vehicleImage.move(speed,0.0);
+		double distance = actualPause*Speed;
+		while (vehicleImage.getX()>-140 && vehicleImage.getX()<1000) {
+			vehicleImage.move(Speed,0.0);
 			pause(50.0);
-			//dlete me later
+			System.out.println(vehicleImage.getX());
+			//delete me later
 			fred.overlaps(vehicleImage, true);
 		}
 		System.out.println("test worked");
@@ -38,6 +31,4 @@ public class Vehicle extends ActiveObject{
   }
   
 	}
-
-}
 
